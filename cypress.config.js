f91aafee1f4891514d75d1b03f5f74b68aed9e4d
Amr Lotfy <amr.lotfy.othman@gmail.com>
2025-04-29 00:11:58 +0300

    use no supportFile for cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:5000" // Set the base URL for your tests
+    baseUrl: "http://localhost:5000",
+    supportFile: false // Disable support file
   }
 });
