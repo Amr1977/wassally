af638062dc72cf9683567beaedf4db44c8d0f5b4
Amr Lotfy <amr.lotfy.othman@gmail.com>
2025-05-03 16:38:26 +0300

    fix cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,13 @@
-
 const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: "http://localhost:5000",
-    supportFile: false // Disable support file
-  }
-});
-
 const createEsbuildPlugin = require("cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:5000",
     specPattern: "**/*.feature",
+    supportFile: false, // Disable support file
     setupNodeEvents(on, config) {
+      // Implement event listeners
       createEsbuildPlugin(on, config);
       return config;
     },
