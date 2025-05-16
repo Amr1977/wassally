808b8f1b14c72ec17c69d418ea9453255247bfc8
Amr Lotfy <amr.lotfy.othman@gmail.com>
2025-05-17 00:11:35 +0300

    refactor, separate constants module from index.js

---
+++ src/modules/constants/constants.js
@@ -0,0 +1,4 @@
+// modules/constants/constants.js
+export const SYSTEM_COMMISSION_RATE = 0.10;  // 10% commission
+export const WAITING_TIMEOUT = 300000;       // 5 minutes in milliseconds
+export const CASH_BLOCK = -100;              // Wallet block threshold
